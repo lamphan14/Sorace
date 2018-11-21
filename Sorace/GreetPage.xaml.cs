0aa967ea85ff18662edc94a8742187de37f3ee69
Lam Phan <ltphan14@gmail.com>
2018-11-21 15:44:53 -0600

    init
    version 0.1

---
+++ Sorace/GreetPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+namespace Sorace
+{
+    [XamlCompilation(XamlCompilationOptions.Skip)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+            slider.Value = 0.5;
+
+        }
+
+
+    }
+}
